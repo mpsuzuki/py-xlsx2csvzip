9663cbb0814f8d1137a880025fcb9ed3037086fe
suzuki toshiya <mpsuzuki@hiroshima-u.ac.jp>
2026-07-19 14:56:41 +0900

    percentile-summaries.py: tool to parse collect-summary.py output

---
+++ percentile-summaries.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import sys
+import argparse
+import zipfile
+import json
+
+from pathlib import Path
+from collections import Counter
+
+
+def parse_args():
+  parser = argparse.ArgumentParser(
+    description="Collect summary.json from ZIP files"
+  )
+  parser.add_argument("json", help="input json file")
+  parser.add_argument("--show-top", type=int, default=10, help="show longest times")
+  return parser.parse_args()
+
+
+def print_percentile(args, backend, smrs):
+  evb = "emit_value_" + backend
+  smrs = sorted(smrs, key=lambda smr: smr["timings"][evb])
+  total = len(smrs)
+  for p in [50, 90, 95, 99, 100]:
+    i = max(0, int(total * p / 100) - 1)
+    smr = smrs[i]
+    print(f"{backend} {p:03d}% #{i:03d} {smr["timings"][evb]:06.2f}")
+
+  for i in range(max(0, total - args.show_top), total):
+    smr = smrs[i]
+    print(f"{backend} #{i:03d} {smr["timings"][evb]:06.2f}\t{smr['xlsx']}\t{smr['_zipfile']}")
+
+  print("")
+
+
+def main():
+  args = parse_args()
+  summaries_all = json.loads( Path(args.json).read_text("utf-8") )
+  summaries = {}
+  for smr in summaries_all:
+    if "backend" not in smr:
+      continue
+    b = smr["backend"]
+    if b in summaries:
+      summaries[b].append(smr)
+    else:
+      summaries[b] = [ smr ]
+
+  for b, smrs in summaries.items():
+    print_percentile(args, b, smrs)
+
+
+if __name__ == "__main__":
+  main()
+
