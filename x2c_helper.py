184350b69554637b175aba7f8edfe4e623f972be
suzuki toshiya <mpsuzuki@hiroshima-u.ac.jp>
2026-07-19 19:27:21 +0900

    x2c_helper.py: simple module for shared functions

---
+++ x2c_helper.py
@@ -0,0 +1,79 @@
+import sys
+import csv
+import shutil
+from pathlib import Path
+from contextlib import contextmanager
+
+# -----------------------------------------------------------------
+# translator from None to ""
+
+def iter_cell_rows(ws):
+  for row in ws.iter_rows():
+    yield ["" if cell.value is None else cell.value for cell in row]
+
+# -----------------------------------------------------------------
+# emit csv from 2D-array via csv.writer()
+
+def write_csv(rows, stream):
+  writer = csv.writer(stream)
+  for row in rows:
+    writer.writerow(row)
+
+# -----------------------------------------------------------------
+# sanitizer for filenames
+
+def safe_filename(name):
+  trans = str.maketrans(
+    {
+      "\\": "_",
+      "/": "_",
+      ":": "_",
+      "*": "_",
+      "?": "_",
+      '"': "_",
+      "<": "_",
+      ">": "_",
+      "|": "_",
+    }
+  )
+  return name.translate(trans)
+
+# -----------------------------------------------------------------
+# return Path(dir + basename + suffix)
+
+def compose_output_pathname(dir, basename, suffix):
+  """Switches the target stream based on user arguments."""
+  filename = ".".join([safe_filename(basename), suffix])
+
+  outdir = Path(dir)
+  outfile = outdir / filename
+  return outfile
+
+# -----------------------------------------------------------------
+# return stream to outfile, with appropriate encoding
+
+@contextmanager
+def open_output_stream(outfile, bom = False):
+  print(f"  create {outfile}", file=sys.stderr)
+  if bom:
+    csv_enc = "utf-8-sig"
+  else:
+    csv_enc = "utf-8"
+  with open(outfile, "w", newline="", encoding=csv_enc) as f:
+    yield f
+
+# -----------------------------------------------------------------
+# create a directory and returns its Path object
+
+def ensure_output_dir(dir, recreate=False):
+  outdir = Path(dir)
+  if recreate:
+    shutil.rmtree(dir)
+  outdir.mkdir(parents=True, exist_ok=True)
+  return outdir
+
+# -----------------------------------------------------------------
+# touch a file under specified dir
+
+def touch_stage(dir, stage):
+  Path(dir, stage).touch()
